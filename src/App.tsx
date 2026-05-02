745be15878376f481c576e4f8def8bacc5127eb3
inmocolabpisos-ship-it <inmocolabpisos@gmail.com>
2026-05-02 21:03:33 +0200

    feat: add App.tsx and index.css for React application

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n/config'
+
+function App() {
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Bienvenido a NEXURA</div>} />
+        </Routes>
+      </Router>
+    </I18nextProvider>
+  )
+}
+
+export default App
